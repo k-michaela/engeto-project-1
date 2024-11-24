11de68a3772d94f21af96cc081b587abb6aae579
k-michaela <michaela.kerberova@gmail.com>
2024-11-24 18:14:49 +0100

    user credentials

--- engeto-project-1.py
+++ engeto-project-1.py
@@ -1,1 +1,15 @@
+"""
+projekt_1.py: první projekt do Engeto Online Python Akademie
 
+author: Michaela Jochcová (Kerberová)
+email: michaela.kerberova@gmail.com
+discord: k.michaela
+"""
+
+# valid user credentials
+registered_users = {
+"bob": "123",
+"ann": "pass123",
+"mike": "password123",
+"liz": "pass123"
+}
