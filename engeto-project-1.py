baa06c7ec07a0c5a461bb55679aa834ac099e715
k-michaela <michaela.kerberova@gmail.com>
2024-11-24 18:22:26 +0100

    username login

--- engeto-project-1.py
+++ engeto-project-1.py
@@ -13,3 +13,18 @@ registered_users = {
 "mike": "password123",
 "liz": "pass123"
 }
+
+# user fills login name and password
+username = input("username: ")
+password = input("password: ")
+
+separator = "-" * 40
+print(separator)
+
+# credentials check
+if username in registered_users and password == registered_users[username]:
+    print(f"Welcome to the app, {username}", "We have 3 texts to analyze.", sep="\n")
+else:
+    print("unregistered user, terminating the program..")
+
+print(separator)
